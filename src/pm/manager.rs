67810f6ec49c39d3995c91eaac92a5d3ff78f938
Dylan McKay <dylanmckay34@gmail.com>
2015-09-28 05:55:57 +1300

    Finished basic pass infrastructure

--- src/pm/manager.rs
+++ src/pm/manager.rs
@@ -1,19 +1,20 @@
 
-use pm::{self,Pass,Id};
+use pm::{self,PassKind,Pass,PassMut,Id};
 use lang;
+use std;
 
-struct PassInfo
+pub struct PassInfo<M: lang::Module>
 {
-    pass: Box<Pass>,
+    pass: PassKind<M>,
     depends: &'static [Id],
 }
 
-pub struct Manager
+pub struct Manager<M: lang::Module>
 {
-    passes: Vec<PassInfo>,
+    passes: Vec<PassInfo<M>>,
 }
 
-impl Manager
+impl<M: lang::Module> Manager<M>
 {
     pub fn new() -> Self {
         Manager {
@@ -21,20 +22,33 @@ impl Manager
         }
     }
 
-    pub fn add(&mut self, pass: Box<Pass>) {
-        if self.passes.iter().any(|a| a.pass.id() == pass.id()) {
-            panic!("we do not currently support multiple passes of the same type");
-        }
+    pub fn add<P>(&mut self, pass: P)
+        where P: pm::PassMetadata, Box<P>: Into<PassKind<M>> {
+
+        use pm::PassMetadata;
+
+        let depends = pass.dependencies();
+        let boxed = Box::new(pass);
 
         self.passes.push(PassInfo {
-            depends: pass.dependencies(),
-            pass: pass,
+            depends: depends,
+            pass: boxed.into(),
         });
     }
 
-    pub fn run<M>(&self, module: &mut M)
-        where M: lang::Module {
-
-
+    pub fn passes<'a>(&'a self) -> std::slice::Iter<'a,PassInfo<M>> {
+        self.passes.iter()
     }
+
+    pub fn run(&self, module: &mut M) {
+    }
+}
+
+/// Builds a list of passes to be run in order.
+pub fn build_pass_list<M>(passes: Vec<PassInfo<M>>,
+                          passes_mut: Vec<PassInfo<M>>)
+    -> Vec<Id>
+    where M: lang::Module {
+    unimplemented!();
+
 }
--- src/pm/mod.rs
+++ src/pm/mod.rs
@@ -7,6 +7,12 @@ use lang;
 /// A pass identifier.
 pub type Id = u64;
 
+pub enum PassKind<M: lang::Module>
+{
+    Immutable(Box<Pass<M>>),
+    Mutable(Box<PassMut<M>>),
+}
+
 /// A pass over a set of instructions.
 pub trait PassMetadata
 {
@@ -17,53 +23,71 @@ pub trait PassMetadata
     fn dependencies(&self) -> &'static [Id] {
         &[]
     }
+
+    /// Checks if the pass is mutable.
+    fn is_mutable(&self) -> bool { false }
 }
 
-pub trait Pass : PassMetadata
+pub trait Pass<M> : PassMetadata
+    where M: lang::Module
 {
-    fn run_module<M>(&mut self, module: &M)
-        where M: lang::Module {
+    fn run_module(&mut self, module: &M) {
 
         for func in module.functions() {
             self.run_function(func);
         }
     }
 
-    fn run_function<F>(&mut self, function: &F)
-        where F: lang::Function {
+    fn run_function(&mut self, function: &M::Function) {
+        use lang::Function;
         
         for bb in function.basic_blocks() {
             self.run_block(bb);
         }
     }
 
-    fn run_block<B>(&mut self, block: &B)
-        where B: lang::BasicBlock {
+    fn run_block(&mut self, block: &<M::Function as lang::Function>::BasicBlock) {
         panic!("the pass is not implemented");
     }
 }
 
-pub trait PassMut : PassMetadata
+pub trait PassMut<M> : PassMetadata
+    where M: lang::Module
 {
-    fn run_module<M>(&mut self, module: &mut M)
-        where M: lang::Module {
+    fn run_module(&mut self, module: &mut M) {
 
         for func in module.functions_mut() {
             self.run_function(func);
         }
     }
 
-    fn run_function<F>(&mut self, function: &mut F)
-        where F: lang::Function {
-        
+    fn run_function(&mut self, function: &mut M::Function) {
+        use lang::Function;
+
         for bb in function.basic_blocks_mut() {
             self.run_block(bb);
         }
     }
 
-    fn run_block<B>(&mut self, block: &mut B)
-        where B: lang::BasicBlock {
+    fn run_block(&mut self, block: &mut <M::Function as lang::Function>::BasicBlock) {
         panic!("the pass is not implemented");
+    }
+
+    fn is_mutable(&self) -> bool { true }
+}
+
+impl<M> Into<PassKind<M>> for Box<Pass<M>>
+    where M: lang::Module {
+
+    fn into(self) -> PassKind<M> {
+        PassKind::Immutable(self)
     }
 }
 
+impl<M> Into<PassKind<M>> for Box<PassMut<M>>
+    where M: lang::Module {
+
+    fn into(self) -> PassKind<M> {
+        PassKind::Mutable(self)
+    }
+}
