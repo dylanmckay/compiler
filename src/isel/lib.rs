b11bf76fec5c71cee48c04dbccd229f793a4ff75
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-04-15 22:39:52 +1200

    Added isel::Type

--- src/isel/lib.rs
+++ src/isel/lib.rs
@@ -1,10 +1,12 @@
 pub use self::opcodes::OpCode;
 pub use self::node::Node;
 pub use self::dag::Dag;
+pub use self::ty::Type;
 
 pub mod opcodes;
 pub mod node;
 pub mod dag;
+pub mod ty;
 
 extern crate num;
 extern crate compiler_ir as ir;
---
+++ src/isel/ty.rs
@@ -0,0 +1,5 @@
+#[derive(Clone,PartialEq,Eq)]
+pub enum Type
+{
+    Integer { bit_size: u16 }
+}
