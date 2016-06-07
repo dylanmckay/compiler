6a31fed8d3c0195e224465d359f5de74dbd1dc6e
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-06-07 14:53:46 +1200

    [mir] Replace 'Local' with 'Register'

--- src/mir/value.rs
+++ src/mir/value.rs
@@ -1,34 +1,50 @@
 use Type;
 
+/// A register.
+/// Stores the zero-based index of the node in the
+/// DAG that is referred to.
+#[derive(Clone,Debug,PartialEq,Eq)]
+pub struct Register {
+    /// The number of the node that is referred to.
+    /// Zero based.
+    pub number: u32,
+    /// The number of the result from the node.
+    pub value_number: u32,
+    /// The type.
+    pub ty: Type,
+}
+
 #[derive(Clone,Debug,PartialEq,Eq)]
 pub enum Value
 {
-    Local {
-        number: u32,
-        ty: Type,
-    },
+    /// An argument to the function.
     Argument {
         number: u32,
         ty: Type,
     },
+    /// A constant integer.
     ConstantInteger {
         bit_width: u32,
         value: i64,
     },
+    /// A register.
+    Register(Register),
 }
 
 impl Value
 {
+    /// Gets the type of the value.
     pub fn ty(&self) -> Type {
         match *self {
-            Value::Local { ref ty, .. } => ty.clone(),
             Value::Argument { ref ty, .. } => ty.clone(),
             Value::ConstantInteger { bit_width, .. } => {
                 Type::Integer { bit_width: bit_width }
             },
+            Value::Register(ref reg)  => reg.ty.clone(),
         }
     }
 
+    /// Creates a new n-bit constant integer.
     pub fn i(width: u32, value: i64) -> Self {
         Value::ConstantInteger {
             bit_width: width,
