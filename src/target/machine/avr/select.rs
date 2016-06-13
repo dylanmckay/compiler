901faf48e2a4444d5fc54a422e92d22b751aada3
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-06-14 00:52:04 +1200

    Add pattern for the AVR add with immediate instruction

--- src/target/machine/avr/select.rs
+++ src/target/machine/avr/select.rs
@@ -30,6 +30,7 @@ macro_rules! operands {
     }
 }
 
+/// An instruction which takes a destination and source GPR8.
 macro_rules! inst_rdrr {
     ($opcode:ident) => {
         pattern! {
@@ -43,14 +44,30 @@ macro_rules! inst_rdrr {
     }
 }
 
+/// An instruction which takes a GPR8 and an 8-bit immediate.
+macro_rules! inst_rdi {
+    ($opcode:ident) => {
+        pattern! {
+            node!($opcode,
+                  operands!(
+                      select::PatternOperand::Value(PatternOperand::Register(&registers::GPR8)),
+                      select::PatternOperand::Value(PatternOperand::Immediate { width: 8 })
+                  )
+            )
+        }
+    }
+}
+
 pub fn selector() -> machine::Selector {
     machine::Selector::new(self::patterns())
 }
 
 pub fn patterns() -> Vec<Pattern> {
     vec![
-        inst_rdrr!(Add),
-        inst_rdrr!(Sub),
+        inst_rdi!(Add),  // ADDRdK
+
+        inst_rdrr!(Add), // ADDRdRr
+        inst_rdrr!(Sub), // SUBRdRr
         pattern! { node!(Ret) },
     ]
 }
