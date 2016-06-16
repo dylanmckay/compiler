0458abe1ace92e1c9ca4a14fa574ff0922bb5ebb
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-06-17 01:24:31 +1200

    Add regalloc crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,32 @@
 extern crate num;
 extern crate bit_vec;
 
-/// Various utilities.
-pub use self::compiler_util as util;
 /// The intermediate representation.
 pub use self::compiler_ir as ir;
-/// The machine level IR.
-pub use self::compiler_mir as mir;
-/// The backend module.
-pub use self::compiler_target as target;
 /// The machine module.
 pub use self::compiler_machine as machine;
+/// The machine level IR.
+pub use self::compiler_mir as mir;
 /// The pass infrastructure.
 pub use self::compiler_pass as pass;
+/// The register allocator.
+pub use self::compiler_regalloc as regalloc;
 /// The instruction selector.
 pub use self::compiler_select as select;
+/// The backend.
+pub use self::compiler_target as target;
 /// The integrated tester.
 pub use self::compiler_test as test;
+/// Various utilities.
+pub use self::compiler_util as util;
 
-pub extern crate compiler_util;
 pub extern crate compiler_ir;
-pub extern crate compiler_mir;
-pub extern crate compiler_target;
 pub extern crate compiler_machine;
+pub extern crate compiler_mir;
 pub extern crate compiler_pass;
+pub extern crate compiler_regalloc;
 pub extern crate compiler_select;
+pub extern crate compiler_target;
 pub extern crate compiler_test;
+pub extern crate compiler_util;
+
