9577f42966cf313bb07780b27ceeff0d4d14c598
Dylan McKay <dylanmckay34@gmail.com>
2016-12-28 14:30:22 +1300

    Reexport the subcrates better

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,20 @@
 /// The intermediate representation.
-pub use self::compiler_ir as ir;
+pub extern crate compiler_ir as ir;
 /// The machine module.
-pub use self::compiler_machine as machine;
+pub extern crate compiler_machine as machine;
 /// The machine level IR.
-pub use self::compiler_mir as mir;
+pub extern crate compiler_mir as mir;
 /// The pass infrastructure.
-pub use self::compiler_pass as pass;
+pub extern crate compiler_pass as pass;
 /// The register allocator.
-pub use self::compiler_regalloc as regalloc;
+pub extern crate compiler_regalloc as regalloc;
 /// The instruction selector.
-pub use self::compiler_select as select;
+pub extern crate compiler_select as select;
 /// The new instruction selector.
-pub use self::compiler_select as select2;
+pub extern crate compiler_select2 as select2;
 /// The backend.
-pub use self::compiler_target as target;
+pub extern crate compiler_target as target;
 /// The integrated tester.
-pub use self::compiler_test as test;
+pub extern crate compiler_test as test;
 /// Various utilities.
-pub use self::compiler_util as util;
-
-pub extern crate compiler_ir;
-pub extern crate compiler_machine;
-pub extern crate compiler_mir;
-pub extern crate compiler_pass;
-pub extern crate compiler_regalloc;
-pub extern crate compiler_select;
-pub extern crate compiler_target;
-pub extern crate compiler_test;
-pub extern crate compiler_util;
-
+pub extern crate compiler_util as util;
